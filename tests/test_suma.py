6a1a0449bf0d4088d48781ca35a6961f6ea4ef59
josmirpa <71086685+josmirpa@users.noreply.github.com>
2022-02-24 20:41:46 +0100

    Update test_suma.py

--- tests/test_suma.py
+++ tests/test_suma.py
@@ -1,18 +1,13 @@
-from app.operaciones import suma
-from app.rot13_cipher import rot13
+from app.operaciones import rot13
 
-
-class TestClass:
-    def test_rot13(self):
-        assert rot13("a") == "n"
-        assert rot13("b") == "o"
 
 
 class TestClass:
     def test_suma(self):
-        assert suma(4, 5) == 9
-        assert suma(-1, -2) == -3
-        assert suma(-7, 5) == -2
-        assert suma(-7, 9) == 2
+        assert rot13("a") == "n"
+        assert rot13("b") == "o"
+
+
+
 
         
