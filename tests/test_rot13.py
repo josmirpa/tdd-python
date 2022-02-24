782b28171b551fab424374151715ac40bfd5b186
josmirpa <71086685+josmirpa@users.noreply.github.com>
2022-02-24 20:08:09 +0100

    Update test_rot13.py

--- tests/test_rot13.py
+++ tests/test_rot13.py
@@ -1,9 +1,6 @@
-from app.operaciones import suma
+from app.operaciones import rot13
 
 
 class TestClass:
     def test_suma(self):
-        assert suma(4, 5) == 9
-        assert suma(-1, -2) == -3
-        assert suma(-7, 5) == -2
-        assert suma(-7, 9) == 2
+        assert rot13('A') == 'N'
