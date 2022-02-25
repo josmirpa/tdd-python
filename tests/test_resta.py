c89c4e8f66a62818e7df0fec0d99c16db9e437d7
josmirpa <71086685+josmirpa@users.noreply.github.com>
2022-02-25 15:57:08 +0100

    Update test_resta.py

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -4,7 +4,7 @@ from app.operaciones import resta
 
 class TestClass:
     def test_resta(self):
-        assert suma(5, 4) == 1
-        assert suma(2, 1) == 1
-        assert suma(7, 5) == 2
-        assert suma(9, 7) == 2
+        assert resta(5, 4) == 1
+        assert resta(2, 1) == 1
+        assert resta(7, 5) == 2
+        assert resta(9, 7) == 2
