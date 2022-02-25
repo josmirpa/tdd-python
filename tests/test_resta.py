00cc1abc515bbe166b6409a0302486f19b195dac
josmirpa <71086685+josmirpa@users.noreply.github.com>
2022-02-25 15:55:44 +0100

    Create test_resta.py

---
+++ tests/test_resta.py
@@ -0,0 +1,10 @@
+from app.operaciones import resta
+
+
+
+class TestClass:
+    def test_resta(self):
+        assert suma(5, 4) == 1
+        assert suma(2, 1) == 1
+        assert suma(7, 5) == 2
+        assert suma(9, 7) == 2
